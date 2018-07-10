19641b518201becb5f657d95704637f92c7c988f
Will Angell-James <william.angelljames9@gmail.com>
2018-07-10 11:34:05 -0400

    Added user.delete, find, save, findAll() in UserServices

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -4,6 +4,18 @@ import webdev.models.User;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+  Optional<User> findById(int userId);
+
+  List<User> findUserByCredentials(String username, String password);
+
+  List<User> findAll();
+
+  User save(User user);
+
+  void deleteById(int id);
 }
