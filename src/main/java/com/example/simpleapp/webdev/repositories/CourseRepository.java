b461d25e18e8f73ce4d8b31a2faaf7fc18cb446e
Will Angell-James <william.angelljames9@gmail.com>
2018-07-15 12:45:08 -0400

    adjusted extends parametres

--- src/main/java/com/example/simpleapp/webdev/repositories/CourseRepository.java
+++ src/main/java/com/example/simpleapp/webdev/repositories/CourseRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository  extends CrudRepository<Integer, Course> {
+public interface CourseRepository  extends CrudRepository<Course, Integer> {
 
 }
