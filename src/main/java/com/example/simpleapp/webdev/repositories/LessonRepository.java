d152387a8e2a6f82ff0e7690fe03d85655b3027f
Will Angell-James <william.angelljames9@gmail.com>
2018-07-23 18:19:05 -0400

    final commit

--- src/main/java/com/example/simpleapp/webdev/repositories/LessonRepository.java
+++ src/main/java/com/example/simpleapp/webdev/repositories/LessonRepository.java
@@ -3,7 +3,9 @@ package com.example.simpleapp.webdev.repositories;
 import com.example.simpleapp.webdev.models.Lesson;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LessonRepository extends CrudRepository<Lesson, Integer>{
 
 }
--- src/main/java/com/example/simpleapp/webdev/repositories/ModuleRepository.java
+++ src/main/java/com/example/simpleapp/webdev/repositories/ModuleRepository.java
@@ -3,6 +3,9 @@ package com.example.simpleapp.webdev.repositories;
 import com.example.simpleapp.webdev.models.Module;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Repository
 public interface ModuleRepository extends CrudRepository<Module, Integer> {
 }
